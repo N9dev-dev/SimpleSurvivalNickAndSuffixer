dc6d433bdc68a2521410b08a01645489ccc64c48
N9dev-dev <81910680+N9dev-dev@users.noreply.github.com>
2023-04-06 06:28:51 +0400

    Added more presets

--- js/script.js
+++ js/script.js
@@ -33,7 +33,19 @@ const nickName = document.getElementById("nickname"),
         // Davi
         8: {
             colors: ["00c0cc", "cc00c4", "cc00c4"]
-        }
+        },
+        // Crock
+        9: {
+            colors: ["ff2aea", "ff3fb0", "ff5575", "ff6a3b", "ff7f00"]
+        },
+        // Cookie
+        10: {
+            colors: ["25ffe5", "1fccea", "1899ef", "1266f5", "0b33fa", "0500ff"]
+        },
+        // mochi
+        11: {
+            colors: ["90d35b", "a6de75", "bce890", "d1f3aa", "e7fdc4"]
+        },
     },
     formats = {
         0: {
